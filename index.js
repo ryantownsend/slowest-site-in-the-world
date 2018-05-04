1650f792bdcbf2d837595206680fc7cafb54bdfb
Ryan Townsend <ryan@ryantownsend.co.uk>
2018-05-04 13:12:44 +0100

    Fixed express

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
 
+// serve static files from the public folder
 app.use(express.static('public'))
 
+// listen on the defined port
 app.listen(PORT, () => console.log(`App booted on port ${PORT}`))
