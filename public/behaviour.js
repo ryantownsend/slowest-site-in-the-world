1f753c02af89e02449b80b8e47faf02118692d0d
Ryan Townsend <ryan@ryantownsend.co.uk>
2018-05-04 19:21:14 +0100

    Loaded jQuery over HTTPS

--- public/behaviour.js
+++ public/behaviour.js
@@ -1,6 +1,6 @@
 (function(d) {
   var tag = d.createElement("script");
-  tag.src = "http://code.jquery.com/jquery-3.3.1.min.js";
+  tag.src = "https://code.jquery.com/jquery-3.3.1.min.js";
   tag.onload = function() {
     console.log("jQuery loaded");
   }
