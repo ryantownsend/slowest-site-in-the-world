11ce78665507227a338c4161be0e8c09b5ac81ee
Ryan Townsend <ryan@ryantownsend.co.uk>
2018-05-05 22:09:11 +0100

    Updated compression ordering as per master

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const compression = require('compression')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+// compress with GZIP
+app.use(compression({ threshold: 0 }))
+
 // serve static files from the public folder
 app.use(express.static('public'))
-
-// compress with GZIP
-app.use(compression())
 
 // listen on the defined port
 app.listen(PORT, () => console.log(`App booted on port ${PORT}`))
