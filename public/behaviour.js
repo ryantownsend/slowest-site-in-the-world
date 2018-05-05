631789c9c44b6e9948a877bd126fab198a13077b
Ryan Townsend <ryan@ryantownsend.co.uk>
2018-05-05 21:10:33 +0100

    Worsened jQuery loading

--- public/behaviour.js
+++ public/behaviour.js
@@ -1,8 +1,1 @@
-(function(d) {
-  var tag = d.createElement("script");
-  tag.src = "https://code.jquery.com/jquery-3.3.1.min.js";
-  tag.onload = function() {
-    console.log("jQuery loaded");
-  }
-  d.head.appendChild(tag);
-})(document)
+console.log("jQuery loaded");
